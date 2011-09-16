de75452ff23a8f9055b6524ea024c21deb1bcc63
Quinn <chaffeqa@gmail.com>
2011-09-16 13:59:29 -0400

    fixed Uv#themes path expansion

--- lib/uv.rb
+++ lib/uv.rb
@@ -12,7 +12,7 @@ module Uv
   self.syntax_path   = File.join(File.dirname(__FILE__), '..', 'syntax')
   self.render_path   = File.join(File.dirname(__FILE__), '..', 'render')
   #self.theme_path    = File.join(render_path, 'xhtml', 'files', 'css')
-  self.theme_path    = File.join(File.dirname(__FILE__), '..', 'app', 'assets', 'stylesheets')
+  self.theme_path    = File.join(File.dirname(__FILE__), '..', 'vendor', 'assets', 'stylesheets')
   self.default_style = 'mac_classic'
   self.syntaxes      = {}
   
